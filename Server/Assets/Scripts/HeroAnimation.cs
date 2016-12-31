8cd9245da4e2d20ddadea0d1fbbab5e2304e0318
Matic Leva <vertical3life@gmail.com>
2016-12-31 16:21:02 +0100

    Imported Unity assets

---
+++ Server/Assets/Scripts/HeroAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class HeroAnimation : MonoBehaviour {
+
+    Animator animator;
+
+    public void Idle()
+    {
+        animator.SetTrigger("Idle");
+    }
+
+    public void Taunt()
+    {
+        animator.SetTrigger("Taunt");
+    }
+
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+}
