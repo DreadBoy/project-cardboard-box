41a1b09c22a1114ccd441b3e0f3b0a8333e1048f
Matic Leva <vertical3life@gmail.com>
2017-03-28 07:14:20 +0200

    Player indicator

---
+++ Server/Assets/Scripts/PlayerIndicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIndicator : MonoBehaviour {
+
+    Vector3 position;
+    float time = 0;
+
+	void Start () {
+        position = transform.localPosition;
+	}
+	
+	void Update () {
+        time += Time.deltaTime;
+        transform.localPosition = position + new Vector3(0, Mathf.Sin(time * 2) / 4, 0);
+	}
+}
